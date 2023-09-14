d12a583bd459cf70bef986c6673167da0bc9d78b
hpeide <hpeide@gmail.com>
2023-09-14 13:45:06 +0200

    listing author and categories

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -14,20 +14,34 @@ export function loader({params}) {
             url
           },
           alt
-        }
+        },
+      author -> {
+        name
+      },
+      categories[] -> {
+        title,
+        _id
+      }
     }`
   )
 }
 
 function BlogPage() {
   const post = useLoaderData()
+
   return (
     <article className='Article'>
       <h1>{post.title}</h1>
+      {post.author && <p><small>Written by {post.author.name}</small></p>}
       {post.mainImage && post.mainImage.asset &&
         <img src={post.mainImage.asset.url} alt={post.title} />
       }
       <PortableText value={post.body} />
+      <p>
+        {post.categories && post.categories.map(category => (
+          <small key={category._id}>{category.title}</small>
+        ))}
+      </p>
     </article>
   )
 }
